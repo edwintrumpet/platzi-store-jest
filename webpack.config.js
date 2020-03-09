ad02b702b2949818801b6766eace58290d90f46c
edwin <spark.com.co@gmail.com>
2020-03-09 17:27:18 -0500

    set route paths

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/platzi-store-jest/',
+    publicPath: '/platzi-store-jest',
     filename: 'bundle.js',
   },
   resolve: {
